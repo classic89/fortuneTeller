6d587b8578795fb589e3d9fea56b819b5012f2bb
Megan Wilson <megan.wilson@walmart.com>
2018-01-15 16:23:11 -0600

    adding changes

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -3,11 +3,28 @@ from sanic.response import text
 
 fortuneTeller = Sanic()
 
+# from cassandra.cluster import Cluster
+# cluster = Cluster()
+# session = cluster.connect('demo')
+
+# session.execute("""
+#
+# insert into users (lastname, age, city, email, firstname) values ('Jones', 35, 'Austin', 'bob@example.com', 'Bob')
+#
+# """)
 
 @fortuneTeller.route("/")
 async def hello(request):
-    return text("Hello {}".format(request.args["name"][0]))
+    # result = session.execute("select * from users where lastname='Jones' ")[0]
+    # name = result.firstname
+    # user = input("What is your name?\n")
+    # hello = "Hellooooo"
+    # return text()
+    return text(list.html)
+    #return text(result.firstname, result.age)
 
+# def showMachineList()
+#     return render_template('list.html')
 
 if __name__ == "__main__":
     fortuneTeller.run(host="0.0.0.0", port=8000)
